b3057a495bb8a1acf58f924cace7f684fd79811d
BT_1N3_12 <raidah@nycstudents.net>
2023-10-16 11:06:29 -0400

    2nd

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class LinearEquation {
+import java.util.Scanner;
+public class LinearEquation{
+    Scanner scan = new Scanner(System.in);
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+    public LinearEquation(){
+        System.out.print("Enter coordinate 1: ");
+        String coordinates = scan.nextLine();
+        String strX = coordinates.substring(1, coordinates.indexOf(","));
+        String strY = coordinates.substring((coordinates.indexOf(","))+2, (coordinates.length()-1));
+        x1 = Integer.parseInt(strX);
+        y1 = Integer.parseInt(strY);
+
+        System.out.print("Enter coordinate 2: ");
+        coordinates = scan.nextLine();
+        strX = coordinates.substring(1, coordinates.indexOf(","));
+        strY = coordinates.substring((coordinates.indexOf(","))+2, (coordinates.length()-1));
+        int x2 = Integer.parseInt(strX);
+        int y2 = Integer.parseInt(strY);
+    }
+
+    public double distance(){
+        double distance;
+        distance = Math.pow((y2-y1), 2);
+        distance = distance + Math.pow((x2-x1), 2);
+        distance = Math.sqrt(distance);
+        distance = Math.round(distance * 100) / 100.0;
+        return distance;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+public class LinearEquationRunner {
+    public static void main(String [] args){
+        LinearEquation line = new LinearEquation();
+        System.out.println(line.distance());
+    }
 }
